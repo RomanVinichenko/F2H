d12d856ac4520c1aa48f3633f75b7985ebb8edf9
r.vinichenko <r.vinichenko@disoft.dev>
2022-10-04 11:52:29 +0300

    new page

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,15 @@
-$(function () {});
+let swiper = new Swiper('.different__swiper', {
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        630: {
+        }
+    }
+});
