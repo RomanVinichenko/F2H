8d45b9dc8d7d67ed617ba11d50b4cb832f64eefb
r.vinichenko <r.vinichenko@disoft.dev>
2022-10-07 12:53:26 +0300

    all desktop

--- app/js/index.js
+++ app/js/index.js
@@ -1,14 +1,23 @@
+let popClose = document.querySelector(".rv-popup__close");
+let pop = document.querySelector(".rv-popup");
+
+if (popClose) {
+  popClose.addEventListener('click', function () {
+    pop.classList.remove("rv-popup--active");
+  });
+}
+
 let swiper = new Swiper('.different__swiper', {
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    breakpoints: {
-        630: {
-        }
-    }
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    630: {}
+  }
 });
+
